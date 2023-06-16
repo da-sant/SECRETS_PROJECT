322dc776d04d44c2b43441ce64b89d476f0324d5
da-sant <santicoon@gmail.com>
2023-06-16 15:11:01 -0600

    Add routing method

--- app.js
+++ app.js
@@ -98,12 +98,12 @@ passport.use(
   )
 );
 
-// ---- GET ROUTES
-
+// ---- HOME
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+// GOOGLE AUTHENTICATION
 app.get(
   '/auth/google',
   passport.authenticate('google', { scope: ['profile'] })
@@ -118,6 +118,8 @@ app.get(
   }
 );
 
+// FACEBOOK AUTHENTICATION
+
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
 app.get(
@@ -129,33 +131,95 @@ app.get(
   }
 );
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
+// ----- REGISTER ROUTING
 
-app.get('/register', (req, res) => {
-  res.render('register');
-});
+app
+  .route('/register')
+  .get((req, res) => {
+    res.render('register');
+  })
+  .post(async (req, res) => {
+    try {
+      const registeredUser = await User.register(
+        { username: req.body.username },
+        req.body.password
+      );
 
-app.get('/secrets', (req, res) => {
+      if (registeredUser) {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets');
+        });
+      } else {
+        res.redirect('/register');
+      }
+    } catch (err) {
+      res.send(err);
+    }
+  });
 
-  User.find({ secrets: { $ne: null } })
-    .then((foundUsers) => {
-      res.render('secrets', { usersWithSecrets: foundUsers });
-    })
-    .catch((err) => {
-      console.log(`${err}`);
+// ---- LOGIN ROUTING
+
+app
+  .route('/login')
+  .get((req, res) => {
+    res.render('login');
+  })
+  .post((req, res) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
     });
-});
 
-app.get('/submit', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render('submit');
-  } else {
-    console.log(`Please login`);
-    res.redirect('/login');
-  }
-});
+    req.login(user, (err) => {
+      if (err) {
+        console.log(`There was an error: ${err}`);
+      } else {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets');
+        });
+      }
+    });
+  });
+
+// ----- SUBMIT ROUTING
+
+app
+  .route('/submit')
+  .get((req, res) => {
+    if (req.isAuthenticated()) {
+      res.render('submit');
+    } else {
+      console.log(`Please login`);
+      res.redirect('/login');
+    }
+  })
+  .post((req, res) => {
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id)
+      .then((foundUser) => {
+        if (foundUser) {
+          foundUser.secrets.push(submittedSecret);
+          foundUser
+            .save()
+            .then(() => {
+              res.redirect('/secrets');
+            })
+            .catch((err) => {
+              console.log(`${err}`);
+            });
+        } else {
+          res.send(`No secrets found ${err}`);
+        }
+      })
+      .catch((err) => {
+        console.log(`There was a problem: ${err}`);
+      });
+  });
+
+//------- LOGOUT
 
 app.get('/logout', (req, res, next) => {
   req.logout((err) => {
@@ -167,71 +231,19 @@ app.get('/logout', (req, res, next) => {
   });
 });
 
-// POST
+// ----- SECRETS
 
-app.post('/submit', (req, res) => {
-  const submittedSecret = req.body.secret;
-
-  console.log(req.user.id);
-
-  User.findById(req.user.id)
-    .then((foundUser) => {
-      if (foundUser) {
-        foundUser.secrets.push(submittedSecret);
-        foundUser
-          .save()
-          .then(() => {
-            res.redirect('/secrets');
-          })
-          .catch((err) => {
-            console.log(`${err}`);
-          });
-      } else {
-        res.send(`No secrets found ${err}`);
-      }
+app.get('/secrets', (req, res) => {
+  User.find({ secrets: { $ne: null } })
+    .then((foundUsers) => {
+      res.render('secrets', { usersWithSecrets: foundUsers });
     })
     .catch((err) => {
-      console.log(`There was a problem: ${err}`);
+      console.log(`${err}`);
     });
 });
 
-app.post('/register', async (req, res) => {
-  try {
-    const registeredUser = await User.register(
-      { username: req.body.username },
-      req.body.password
-    );
-
-    if (registeredUser) {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/secrets');
-      });
-    } else {
-      res.redirect('/register');
-    }
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-app.post('/login', (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-
-  req.login(user, (err) => {
-    if (err) {
-      console.log(`There was an error: ${err}`);
-    } else {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/secrets');
-      });
-    }
-  });
-});
-
-//Start the server on port 3000 and log a message to the console
+//-- SERVER PORT 3000
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
